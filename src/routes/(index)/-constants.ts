e3e85165c9b56c730ffe665b07098312aba01312
Kevin Peredo <kveperedo@gmail.com>
2026-05-27 14:26:57 +0800

    Refactor index meta to constants

--- src/routes/(index)/-constants.ts
+++ src/routes/(index)/-constants.ts
@@ -4,3 +4,24 @@ export const SECTION_IDS = {
   SUMMARY: "summary",
   INFO: "info",
 } as const;
+
+export const META: Array<React.JSX.IntrinsicElements["meta"]> = [
+  { title: "Portfolio | Kevin Von Erich Peredo" },
+  { property: "og:url", content: "https://www.kevinperedo.com/" },
+  { property: "og:type", content: "website" },
+  { property: "og:title", content: "Portfolio | Kevin Von Erich Peredo" },
+  {
+    property: "og:description",
+    content: "A full-time software engineer working at Lexagle Inc. in the Philippines.",
+  },
+  { property: "og:image", content: "/og-image.png" },
+  { name: "twitter:card", content: "summary_large_image" },
+  { property: "twitter:domain", content: "kevinperedo.com" },
+  { property: "twitter:url", content: "https://www.kevinperedo.com/" },
+  { name: "twitter:title", content: "Portfolio | Kevin Von Erich Peredo" },
+  {
+    name: "twitter:description",
+    content: "A full-time software engineer working at Lexagle Inc. in the Philippines.",
+  },
+  { name: "twitter:image", content: "/og-image.png" },
+];
--- src/routes/(index)/index.tsx
+++ src/routes/(index)/index.tsx
@@ -3,35 +3,14 @@ import { createFileRoute } from "@tanstack/react-router";
 import { FadeIn } from "./-components/fade-in";
 import { LinkButtons } from "./-components/link-buttons";
 import { ScrollButton } from "./-components/scroll-button";
-import { SECTION_IDS } from "./-constants";
+import { META, SECTION_IDS } from "./-constants";
 import { InfoSection } from "./-sections/info";
 import { IntroductionSection } from "./-sections/introduction";
 import { ProjectsSection } from "./-sections/projects";
 import { SummarySection } from "./-sections/summary";
 
 export const Route = createFileRoute("/(index)/")({
-  head: () => ({
-    meta: [
-      { title: "Portfolio | Kevin Von Erich Peredo" },
-      { property: "og:url", content: "https://www.kevinperedo.com/" },
-      { property: "og:type", content: "website" },
-      { property: "og:title", content: "Portfolio | Kevin Von Erich Peredo" },
-      {
-        property: "og:description",
-        content: "A full-time software engineer working at Lexagle Inc. in the Philippines.",
-      },
-      { property: "og:image", content: "/og-image.png" },
-      { name: "twitter:card", content: "summary_large_image" },
-      { property: "twitter:domain", content: "kevinperedo.com" },
-      { property: "twitter:url", content: "https://www.kevinperedo.com/" },
-      { name: "twitter:title", content: "Portfolio | Kevin Von Erich Peredo" },
-      {
-        name: "twitter:description",
-        content: "A full-time software engineer working at Lexagle Inc. in the Philippines.",
-      },
-      { name: "twitter:image", content: "/og-image.png" },
-    ],
-  }),
+  head: () => ({ meta: META }),
   component: Index,
 });
 
